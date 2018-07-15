f08016b97ec4d15e1f50433b3daee7a0a84aee0e
jonatanavarrete <root@descolonizado.localdomain>
2018-07-15 19:31:11 +0200

    First commit Telegram Bot - BASIC

---
+++ lePaveBot.py
@@ -0,0 +1,29 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+TOKEN = "621296017:AAGjhIBQmgNYW1cZ0BhcY1t0K0N3pSPZcjQ"
+
+def start(bot, update):
+  update.message.reply_text("Bot sobre ciclismo creado por Jonatan Navarrete")
+
+def convert_uppercase(bot, update):
+  update.message.reply_text(update.message.text.upper())
+
+def main():
+  # Create Updater object and attach dispatcher to it
+  updater = Updater(TOKEN)
+  dispatcher = updater.dispatcher
+  print("Bot started")
+
+  # Add command handler to dispatcher
+  start_handler = CommandHandler('start',start)
+  upper_case = MessageHandler(Filters.text, convert_uppercase)
+  dispatcher.add_handler(start_handler)
+  dispatcher.add_handler(upper_case)
+
+  # Start the bot
+  updater.start_polling()
+
+  # Run the bot until you press Ctrl-C
+  updater.idle()
+
+if __name__ == '__main__':
+  main()
